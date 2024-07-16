17c63a707c138f29aca830a76fa4d47ef7e98e18
Ali <AliHasanJMM@gmail.com>
2024-07-16 19:43:55 +0300

    ADDED ERROR HANDLER

---
+++ handlers/error.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+	"text/template"
+	"ASCII-GEN/structs"
+)
+
+func renderErrorPage(w http.ResponseWriter, errMsg string, statusCode int) {
+	w.WriteHeader(statusCode)
+	data := structs.PageData{
+		ErrorMessage: errMsg,
+		StatusCode:   strconv.Itoa(statusCode),
+	}
+
+	tmpl, err := template.ParseFiles("./statics/error.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
